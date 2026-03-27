a66d4cde303904c5603142a0045e981a468da88c
CHANDU <155825649+Chandu-challa@users.noreply.github.com>
2026-03-27 11:20:35 +0530

    host issue

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # only for local dev
 
 def get_connection():
     return psycopg2.connect(
-        dbname="customer_db",
-        user="postgres",
-        password="12345678",
-        host=" https://app-server-aigs.onrender.com/db/customers",
-        port="5433"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
