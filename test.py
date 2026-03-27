f4996cadbfe0b7de30498b5f2853643a49b858f9
CHANDU <155825649+Chandu-challa@users.noreply.github.com>
2026-03-27 11:35:24 +0530

    test

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 
-conn = psycopg2.connect(
-    dbname="customer_db",
-    user="postgres",
-    password="12345678",
-    host="127.0.0.1",
-    port="5433"
-)
+load_dotenv()  # only for local dev
 
-print("Connected successfully!")
+def get_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
